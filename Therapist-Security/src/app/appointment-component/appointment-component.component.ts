6c2fd425f42dc28e6e50df0afda17086a0e4bba5
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-10-12 11:10:35 +0530

    Added bootstrap styles

--- Therapist-Security/src/app/appointment-component/appointment-component.component.ts
+++ Therapist-Security/src/app/appointment-component/appointment-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Appointment } from '../appointment.model';
 import * as constants from '../constants';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -9,81 +9,88 @@ import { Observable } from 'rxjs';
   templateUrl: './appointment-component.component.html',
   styleUrls: ['./appointment-component.component.css'],
 })
-
-export class AppointmentComponentComponent {
-  public appointment:Appointment;
-  
+export class AppointmentComponentComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('AppointmentComponentComponent initialized!');
+  }
+  public appointment: Appointment;
 
   constructor(private http: HttpClient) {
-    this.appointment= {
-      id:1,
+    this.appointment = {
+      id: 1,
       therapistId: 1,
-      therapistName : 'Therepist-1',
-      clientId :1,
-      clientName :'Client-1',
-      clientAddress:'Kharadi, Pune',
-      checkinTime:'9:00 a.m.',
+      therapistName: 'Therepist-1',
+      clientId: 1,
+      clientName: 'Client-1',
+      clientAddress: 'Kharadi, Pune',
+      checkinTime: '9:00 a.m.',
       checkoutTime: '11:00 a.m.',
-      actualCheckinTime:'',
+      actualCheckinTime: '',
       actualCheckoutTime: '',
-      status: constants.appointStatusScheduled
+      status: constants.appointStatusScheduled,
     };
   }
 
-  updateAppointment(appointment:Appointment){
-    const headers = { 'content-type': 'application/json'}  
-    const body=JSON.stringify(appointment);
-    console.log(body)
-    return this.http.post(constants.baseURL + 'appointment' + appointment.id +'/', body,{'headers':headers})
-  }
-
-  doGetAppointment(id:number):Observable<Appointment> {
-    const headers = { 'content-type': 'application/json'}  
-    const body=JSON.stringify(id);
+  updateAppointment(appointment: Appointment) {
+    const headers = { 'content-type': 'application/json' };
+    const body = JSON.stringify(appointment);
     console.log(body);
-    return this.http.get<Appointment>(constants.baseURL + 'appointment/'+id +'/')
+    return this.http.post(
+      constants.baseURL + 'appointment' + appointment.id + '/',
+      body,
+      { headers: headers }
+    );
   }
 
-  doCheckIn():void {
+  doGetAppointment(id: number): Observable<Appointment> {
+    const headers = { 'content-type': 'application/json' };
+    const body = JSON.stringify(id);
+    console.log(body);
+    return this.http.get<Appointment>(
+      constants.baseURL + 'appointment/' + id + '/'
+    );
+  }
+
+  doCheckIn(): void {
     this.appointment.status = constants.appointStatusInProgress;
 
-    alert("Calling Update Appointment API with CheckIn");
+    alert('Calling Update Appointment API with CheckIn');
   }
 
-  doCheckOut():void {
+  doCheckOut(): void {
     this.appointment.status = constants.appointStatusCompleted;
-    alert("Calling Update Appointment API with CheckOut");
+    alert('Calling Update Appointment API with CheckOut');
   }
 
-  doSOS():void {
-    this.appointment.status =constants.appointStatusAlert;
-    alert("Calling Update Appointment API with SOS")
+  doSOS(): void {
+    this.appointment.status = constants.appointStatusAlert;
+    alert('Calling Update Appointment API with SOS');
   }
 
-
-  isCheckInDisabled():boolean{
-    if (this.appointment.status !=  constants.appointStatusScheduled) {
-        return true;
+  isCheckInDisabled(): boolean {
+    if (this.appointment.status != constants.appointStatusScheduled) {
+      return true;
     }
     return false;
   }
 
-  isSOSDisabled():boolean{
-    if (this.appointment.status ==  constants.appointStatusScheduled || 
-      this.appointment.status == constants.appointStatusCompleted){
-        return true;
+  isSOSDisabled(): boolean {
+    if (
+      this.appointment.status == constants.appointStatusScheduled ||
+      this.appointment.status == constants.appointStatusCompleted
+    ) {
+      return true;
     }
     return false;
   }
 
-  isCheckoutButtonDisabled():boolean{
-    if (this.appointment.status ==  constants.appointStatusInProgress || 
-      this.appointment.status == constants.appointStatusOverdue){
-        return false;
+  isCheckoutButtonDisabled(): boolean {
+    if (
+      this.appointment.status == constants.appointStatusInProgress ||
+      this.appointment.status == constants.appointStatusOverdue
+    ) {
+      return false;
     }
     return true;
   }
-
-
-
 }
--- Therapist-Security/src/app/dashboard/dashboard.component.ts
+++ Therapist-Security/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
 //import * as bootstrap from 'bootstrap';
-
 
 export interface PeriodicElement {
   name: string;
@@ -10,73 +9,73 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-
-
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-
-  public chart:any;
+export class DashboardComponent implements OnInit {
+  public chart: any;
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
 
   ngOnInit(): void {
-    
     this.createChart();
   }
- 
 
-  getAppointmentsData(){
+  getAppointmentsData() {}
 
-  }
-
-  displayData(row:any){
+  displayData(row: any) {
     console.log(row);
     //his.router.navigate(["/vendor-list/vendor/a/services/a/details/",sid]);
   }
 
-  createChart(){
-    this.chart = new Chart("MyChart", {
+  createChart() {
+    this.chart = new Chart('MyChart', {
       type: 'pie', //this denotes tha type of chart
 
-      data: {// values on X-Axis
-        labels: ['Scheduled', 'Completed','In Progress','Alert','Emergency','Cancelled'],
-	       datasets: [{
-    label: 'Count',
-    data: [20, 11, 4, 2, 1, 2],
-    backgroundColor: [
-      'blue',
-      'yellow',
-      'green',
-      'pink',
-      'red',
-			'black',
-    ],
-    hoverOffset: 4
-  }],
+      data: {
+        // values on X-Axis
+        labels: [
+          'Scheduled',
+          'Completed',
+          'In Progress',
+          'Alert',
+          'Emergency',
+          'Cancelled',
+        ],
+        datasets: [
+          {
+            label: 'Count',
+            data: [20, 11, 4, 2, 1, 2],
+            backgroundColor: [
+              'blue',
+              'yellow',
+              'green',
+              'pink',
+              'red',
+              'black',
+            ],
+            hoverOffset: 4,
+          },
+        ],
       },
       options: {
-        aspectRatio:2.5
-      }
-
+        aspectRatio: 2.5,
+      },
     });
-   
   }
-
 }
